a09bbf3c06ed86d7f838eb9bf0b43b156e329c80
Colby Green <cjdude22@gmail.com>
2016-09-14 08:26:14 -0700

    Finshed Project

--- battleship.js
+++ battleship.js
@@ -62,7 +62,7 @@ var gameBoard = [
 				]
 
 var hexDigits = new Array
-                   ("0","1","2","3","4","5","6","7","8","9", "a", "b","c","d","e","f");
+                   ("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f");
 
 function rgb2hex(rgb) {
 	rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
@@ -83,7 +83,7 @@ function fireTorpedo() {
 	row = letterConversion[row];
   var column = userInput.substring(1,3) - 1;
 	var battleshipGuess = gameBoard[row][column];
-	 var guessDiv = "#s" + row + column;
+	var guessDiv = "#s" + row + column;
 	var rgbHexColor = rgb2hex($(guessDiv).css("background-color"));
 	 if( (rgbHexColor == "#f6f8f9")&& battleshipGuess ){
 		 hitCount += battleshipGuess
